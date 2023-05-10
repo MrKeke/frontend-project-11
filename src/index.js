8b13d079a086abf8301b9e7adbe1ee66bd9df6fe
MrKeke <alexgraf0701@gmail.com>
2023-05-10 13:56:12 +0300

    lastcommit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'bootstrap';
+import app from './application.js';
+import './styles.css';
+
+app();
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,23 @@
-"use strict";
-
-const path = require("path");
-const autoprefixer = require("autoprefixer");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // eslint-disable-line
 
 module.exports = {
-  mode: "development",
-  entry: "./src/js/main.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  experiments: {
-    topLevelAwait: true,
-  },
+  mode: process.env.NODE_ENV || 'development',
   devServer: {
-    static: path.resolve(__dirname, "dist"),
-    port: 8000,
-    hot: true,
+    port: 8001,
     open: true,
+    hot: true,
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
   module: {
     rules: [
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: "style-loader",
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: "css-loader",
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: () => [autoprefixer],
-              },
-            },
-          },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: "sass-loader",
-          },
-        ],
-      },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 };
