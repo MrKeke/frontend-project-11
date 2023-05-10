49256dfcd2fd9af5c02d6f042d44cb01738cc15d
MrKeke <alexgraf0701@gmail.com>
2023-05-10 14:17:07 +0300

    try3

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // eslint-disable-line
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // eslint-disable-line
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // eslint-disable-line
 
 module.exports = {
-  mode: process.env.NODE_ENV || "development",
+  mode: process.env.NODE_ENV || 'development',
   devServer: {
     port: 8001,
     open: true,
@@ -12,14 +12,14 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      template: "index.html",
+      template: 'index.html',
     }),
     new MiniCssExtractPlugin(),
   ],
